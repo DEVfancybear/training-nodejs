0d12a4878f61321aca0503a753e6fdc8116a7476
DEVfancybear <duonghacker2345@gmail.com>
2020-05-05 15:31:31 +0700

    B10

--- Ghi dữ liệu bằng Writable/server.js
+++ Ghi dữ liệu bằng Writable/server.js
@@ -2,8 +2,7 @@ var file = require("fs");
 var docluong = file.createReadStream(__dirname + "/readme.txt", {
   encoding: "utf8",
 });
-var ghiluong = file.createWriteStream(__dirname + "/writeme.txt");
 docluong.on("data", function (chunk) {
-  console.log("Ghi luong nao: ");
-  ghiluong.write(chunk);
+  console.log("Doc luong nao: ");
+  console.log(chunk);
 });
